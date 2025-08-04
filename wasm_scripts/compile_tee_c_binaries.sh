b1386658c4349ec82131af536c70ae50c699e5d2
michaelbunte <michaelwbunte@gmail.com>
2025-08-04 12:01:43 -0700

    hello world commands updated again

--- wasm_scripts/compile_tee_c_binaries.sh
+++ wasm_scripts/compile_tee_c_binaries.sh
@@ -1,11 +1,23 @@
 export NV_TARGET_BOARD=234
-export CROSS_COMPILE_AARCH64_PATH=~/jetpack_5/aarch64--glibc--stable-2023.08-1
-export CROSS_COMPILE_AARCH64=~/jetpack_5/aarch64--glibc--stable-2023.08-1/bin/aarch64-buildroot-linux-gnu-
-export UEFI_STMM_PATH=~/jetpack_5/Linux_for_Tegra/bootloader/standalonemm_optee_t234.bin
+
+
+export CROSS_COMPILE_AARCH64_PATH=~/michael/aarch64--glibc--stable-2023.11-1
+
+
+export CROSS_COMPILE_AARCH64=~/michael/aarch64--glibc--stable-2023.11-1/bin/aarch64-buildroot-linux-gnu-
+
+
+export UEFI_STMM_PATH=~/optee-dependencies/Jetson_Linux_R35.4.1_aarch64/Linux_for_Tegra/bootloader/standalonemm_optee_t234.bin
+
 
 make -C hello_world \
-    CROSS_COMPILE="/home/michael/jetpack_5/aarch64--glibc--stable-2023.08-1/bin/aarch64-buildroot-linux-gnu-" \
-    TA_DEV_KIT_DIR="/home/michael/jetpack_5/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/build/t234/export-ta_arm64/" \
-    OPTEE_CLIENT_EXPORT="/home/michael/jetpack_5/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/install/t234/usr" \
-    TEEC_EXPORT="/home/michael/jetpack_5/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/install/t234/usr" \
-    -j"$(nproc)"
+
+	CROSS_COMPILE="/home/nick2/michael/aarch64--glibc--stable-2023.11-1/bin/aarch64-buildroot-linux-gnu-" \
+
+	TA_DEV_KIT_DIR="/home/nick2/optee-dependencies/Jetson_Linux_R35.4.1_aarch64/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/build/t234/export-ta_arm64/" \
+
+	OPTEE_CLIENT_EXPORT="/home/nick2/optee-dependencies/Jetson_Linux_R35.4.1_aarch64/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/install/t234/usr" \
+
+	TEEC_EXPORT="/home/nick2/optee-dependencies/Jetson_Linux_R35.4.1_aarch64/Linux_for_Tegra/sources/tegra/optee-src/nv-optee/optee/install/t234/usr" \
+
+	-j"$(nproc)"
